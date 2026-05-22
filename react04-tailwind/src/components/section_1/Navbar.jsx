002cbf6efd2ac36a639f3ac71c7d88596f2f4c6c
Aayush Kumar <ayushankitaara@gmail.com>
2026-05-22 11:08:35 +0530

    Learned how to desgib UI in react by breaking it in small functions/features & also gained experienced in Tailwind css

---
+++ react04-tailwind/src/components/section_1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-16 py-6'>
+        <h4 className='bg-black text-white px-6 py-2 rounded-full uppercase'>Target Audience</h4>
+        <button className='bg-gray-200 rounded-full uppercase py-2 px-6 text-sm tracking-widest'>Digital Banking Platfform</button>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ react04-tailwind/src/components/section_2/Section2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div className='w-full h-screen bg-gray-700'>
+        
+    </div>
+  )
+}
+
+export default Section2
